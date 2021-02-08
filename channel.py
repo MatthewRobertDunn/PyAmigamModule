5ab0d5d41d6a7e66ad18c79feae3d498861bf608
Matthew <mrdunn@gmail.com>
2021-02-08 12:12:40 +1030

    Updates to channel.py

--- channel.py
+++ channel.py
@@ -1,3 +1,4 @@
+#Generates audio for a single channel
 class Channel:
     def __init__(self, samples, time_step):
         self.samples = samples
@@ -5,9 +6,21 @@ class Channel:
         self.current_time = 0
         self.sample = -1
         self.period = 0
+    
+    #plays a note using a given sample, at a frequency given by period, with given effect
+    def play_note(self, sample, period, effect):
+        if sample > -1: # start playing a brand new note
+            self.current_time = 0
+            self.sample = sample
+            self.period = period
+            self.effect = effect
+        else: 
+            #only changing effect
+            self.effect = effect
+        return
 
-    def play_note(sample, period, effect):
-        pass
-
-    def get_frames(duration):
+    #returns audio frames for this channel for a given duration in secs.
+    def get_frames(self, duration):
+        #todo        
+        self.current_time += duration
         pass
