fe4738691078cbe9da530ebad6de37c0d3599747
Matthew <mrdunn@gmail.com>
2021-02-09 15:15:52 +1030

    run player in second proccess

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import moduleloader
 import sounddevice as sd
-import time
-import channel as Channel
 import player as Player
+from multiprocessing import Process
 
-sd.default.samplerate = 44100
-sd.default.channels = 1
-sd.default.dtype = 'float32'
+def play_module():
+    sd.default.samplerate = 44100
+    sd.default.channels = 1
+    sd.default.dtype = 'float32'
+    current_time = 0
+    time_step = 1.0 / sd.default.samplerate
 
-current_time = 0
-time_step = 1.0 / sd.default.samplerate
+    song = moduleloader.load("https://api.modarchive.org/downloads.php?moduleid=85375#an_adventurers_tale.mod")
+    player = Player.Player(song,time_step)
+    os = sd.OutputStream()
+    os.start()
 
-song = moduleloader.load("https://api.modarchive.org/downloads.php?moduleid=75502#popcorn_90.mod")
+    while not player.finished:
+        os.write(player.get_frames_row())
 
-player = Player.Player(song,time_step)
-
-os = sd.OutputStream()
-os.start()
-
-while not player.finished:
-    os.write(player.get_frames_row())
-
-input("Press Enter to continue...")
+if __name__ == '__main__':
+    p = Process(target=play_module)
+    p.start()
+    p.join()
+    input("Press Enter to continue...")
